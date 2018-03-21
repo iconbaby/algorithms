0182111e01631786cb5f5dcafc31af738f286260
slkk <644187060@qq.com>
2018-03-21 14:59:58 +0800

    init proj

---
+++ src/com/slkk/Main.java
@@ -0,0 +1,14 @@
+package com.slkk;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        StdOut.print("ddd");
+        String a = StdIn.readString();
+        StdOut.print(a);
+    }
+}
